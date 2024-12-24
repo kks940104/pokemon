47318433db555e6acfa3e1b9cb15e328c6de12d4
kks940104 <kks940104@gmail.com>
2024-12-24 17:34:12 +0900

    약관 관리까지..
    약관 관리까지..

--- src/main/java/org/koreait/admin/basic/controllers/BasicController.java
+++ src/main/java/org/koreait/admin/basic/controllers/BasicController.java
@@ -1,14 +1,18 @@
 package org.koreait.admin.basic.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.koreait.admin.global.menu.MenuDetail;
 import org.koreait.admin.global.menu.Menus;
 import org.koreait.global.annotations.ApplyErrorPage;
 import org.koreait.global.entities.SiteConfig;
+import org.koreait.global.entities.Terms;
 import org.koreait.global.libs.Utils;
 import org.koreait.global.services.CodeValueService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,6 +37,14 @@ public class BasicController {
         return Menus.getMenus(menuCode());
     }
 
+    // region 사이트 기본 정보 설정
+
+    /**
+     * 사이트 기본 정보 설정
+     * 
+     * @param model
+     * @return
+     */
     @GetMapping({"", "/siteConfig"})
     public String siteConfig (Model model) {
         commonProcess("siteConfig", model);
@@ -43,6 +55,13 @@ public class BasicController {
         return "admin/basic/siteConfig";
     }
 
+    /**
+     * 사이트 기본 정보 설정 처리
+     *
+     * @param form
+     * @param model
+     * @return
+     */
     @PatchMapping("/siteConfig")
     public String siteConfigPs(SiteConfig form, Model model) {
         commonProcess("siteConfig", model);
@@ -53,12 +72,58 @@ public class BasicController {
         return "admin/basic/siteConfig"; // 임시
     }
 
+    // endregion
+
+    // region 약관 관리 설정
+
+    /**
+     * 약관 관리 양식, 목록
+     * @param form
+     * @param model
+     * @return
+     */
+
+    @GetMapping("/terms")
+    public String terms(@ModelAttribute Terms form, Model model) {
+        commonProcess("terms", model);
+        return "admin/basic/terms";
+    }
+    /**
+     * 약관 등록 처리
+     * @param form
+     * @param model
+     * @return
+     */
+    @PostMapping("/terms")
+    public String termsPs(@Valid Terms form, Errors errors, Model model) {
+        commonProcess("terms", model);
+
+        if (errors.hasErrors()) {
+            return "admin/basic/terms";
+        }
+        return "admin/basic/terms"; // 임시....
+    }
+
+    // endregion
+
     /**
      * 기본 설정 공통 처리 부분
      * @param mode
      * @param model
      */
     private void commonProcess(String mode, Model model) {
+
+        mode = StringUtils.hasText(mode) ? mode : "siteConfig";
+        String pageTitle = null;
+
+        if (mode.equals("siteConfig")) {
+            pageTitle = "사이트 기본정보";
+        } else if (mode.equals("terms")) {
+            pageTitle = "약관 관리";
+        }
+        pageTitle += " - 기본 설정";
+
+        model.addAttribute("pageTitle", pageTitle);
         model.addAttribute("subMenuCode", mode);
     }
 }
--- src/main/java/org/koreait/global/interceptors/CommonInterceptor.java
+++ src/main/java/org/koreait/global/interceptors/CommonInterceptor.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
 import java.util.Objects;
 
 
@@ -18,14 +19,20 @@ public class CommonInterceptor implements HandlerInterceptor {
 
     private final CodeValueService codeValueService;
 
+/*    private List<String> excludeExtensions = List.of(".css", ".js", ".png", ".gif", ".jpg", ".jpeg", ".pdf", ".txt");*/
+
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-
-        setSiteConfig(modelAndView, request);
+        setSiteConfig(modelAndView);
     }
 
     /* 사이트 설정 */
-    private void setSiteConfig(ModelAndView mv, HttpServletRequest request) {
+    private void setSiteConfig(ModelAndView mv) {
+/*        String uri = request.getRequestURI().toLowerCase();
+
+        boolean matched = excludeExtensions.stream().anyMatch(uri::contains);*/
+        if (mv == null) return;
+
         SiteConfig config = Objects.requireNonNullElseGet(codeValueService.get("siteConfig", SiteConfig.class), SiteConfig::new);
         mv.addObject("siteConfig", config);
     }
