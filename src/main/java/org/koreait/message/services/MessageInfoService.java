08f96642e9eb0c05167a1c3ee44f2fe891a1b8e7
kks940104 <kks940104@gmail.com>
2025-01-03 15:10:19 +0900

    Message 조회 추가
    Message 조회 추가

--- src/main/java/org/koreait/message/services/MessageInfoService.java
+++ src/main/java/org/koreait/message/services/MessageInfoService.java
@@ -1,9 +1,15 @@
 package org.koreait.message.services;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.StringExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.koreait.global.libs.Utils;
 import org.koreait.global.paging.ListData;
+import org.koreait.global.paging.Pagination;
+import org.koreait.member.entities.Member;
+import org.koreait.member.libs.MemberUtil;
 import org.koreait.message.Exceptions.MessageNotFoundException;
 import org.koreait.message.controllers.MessageSearch;
 import org.koreait.message.entities.Message;
@@ -11,6 +17,7 @@ import org.koreait.message.entities.QMessage;
 import org.koreait.message.repositories.MessageRepository;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -20,6 +27,9 @@ import java.util.List;
 public class MessageInfoService {
     private final MessageRepository messageRepository;
     private final JPAQueryFactory queryFactory;
+    private final HttpServletRequest request;
+    private final Utils utils;
+    private final MemberUtil memberUtil;
 
     /**
      * 쪽지 하나 조회
@@ -28,7 +38,22 @@ public class MessageInfoService {
      */
     public Message get(Long seq) {
 
-        Message item = messageRepository.findById(seq).orElseThrow(MessageNotFoundException::new);
+        BooleanBuilder builder = new BooleanBuilder();
+        BooleanBuilder orBuilder = new BooleanBuilder();
+        QMessage message = QMessage.message;
+
+        builder.and(message.seq.eq(seq));
+
+        if (!memberUtil.isAdmin()) {
+            Member member = memberUtil.getMember();
+
+            orBuilder.or(message.sender.eq(member))
+                    .or(message.receiver.eq(member));
+
+            builder.and(orBuilder);
+        }
+
+        Message item = messageRepository.findOne(builder).orElseThrow(MessageNotFoundException::new);
         addInfo(item); // 추가 정보 처리
         return item;
     }
@@ -44,9 +69,40 @@ public class MessageInfoService {
         limit = limit < 1 ? 20 : limit;
         int offset = (page - 1) * limit;
 
-        BooleanBuilder andBuilder = new BooleanBuilder();
+        // region 검색 조건 처리
 
+        BooleanBuilder andBuilder = new BooleanBuilder();
         QMessage message = QMessage.message;
+        String mode = search.getMode();
+        Member member = memberUtil.getMember();
+        mode = StringUtils.hasText(mode) ? mode : "receive";
+        // send - 보낸 쪽지 목록, receive - 받은 쪽지 목록
+        andBuilder.and(mode.equals("send") ? message.sender.eq(member) : message.receiver.eq(member));
+
+        // region 보낸 사람 조건 검색
+
+        List<String> sender = search.getSender();
+        if (mode.equals("receive") && sender != null && !sender.isEmpty()) {
+            andBuilder.and(message.sender.email.in(sender));
+        }
+
+        // endregion
+
+        // region 키워드 검색 처리
+
+        String sopt = search.getSopt();
+        String skey = search.getSkey();
+
+        sopt = StringUtils.hasText(sopt) ? sopt : "ALL";
+        if (StringUtils.hasText(skey)) {
+            StringExpression condition = sopt.equals("SUBJECT") ? message.subject : message.subject.concat(message.content);
+
+            andBuilder.and(condition.contains(skey.trim()));
+        }
+
+        // endregion
+
+        // endregion
 
         List<Message> items = queryFactory.selectFrom(message)
                 .leftJoin(message.receiver)
@@ -60,8 +116,9 @@ public class MessageInfoService {
         items.forEach(this::addInfo); // 추가 정보 처리
 
         long total = messageRepository.count(andBuilder);
+        Pagination pagination = new Pagination(page, (int) total, utils.isMobile() ? 5 : 10 , limit, request);
 
-        return null;
+        return new ListData<>(items, pagination);
     }
 
     /**
