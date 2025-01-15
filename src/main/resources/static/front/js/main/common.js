3da0c9915d32a81402e06dc0e469777b4b967869
kks940104 <kks940104@gmail.com>
2025-01-15 17:52:20 +0900

    main 화면 추가
    main 화면 추가

---
+++ src/main/resources/static/front/js/main/common.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", function() {
+    /* 메인 베너 S */
+    new Swiper(".main-banner .banners", {
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+    /* 메인 베너 E */
+});
